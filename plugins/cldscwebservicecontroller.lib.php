Okhoshi/Claroline.REST-API-Plugin

commit 7cd8fb6413f7d14e03f9415ec4963f75e92e5365

Replace the variable descriptions categories to the last position

plugins/cldscwebservicecontroller.lib.php
@@ -30,8 +30,16 @@ class CLDSCWebServiceController
 		
 		$dscList = array();
 		
-		foreach ( course_description_get_item_list($cid) as $item )
+		$array = course_description_get_item_list($cid);
+		$lastCat = max(array_map(function($row) { return $row['category']; }, $array)) + 1;
+		
+		foreach ( $array as $item )
 		{
+			if ( $item['category'] == -1 )
+			{
+				$item['title'] = get_lang('Other');
+				$item['category'] = $lastCat;
+			}
 			$item['content'] = trim(strip_tags($item['content']));
 			$item['visibility'] = ($item['visibility'] != 'HIDE');
 			$item['resourceId'] = $item['id'];

commit d78258c0fd2fc8874df956eb588fac3f8031ac91

Fix bug introduced with 7cd8fb6

plugins/cldscwebservicecontroller.lib.php
@@ -31,7 +31,10 @@ class CLDSCWebServiceController
 		$dscList = array();
 		
 		$array = course_description_get_item_list($cid);
-		$lastCat = max(array_map(function($row) { return $row['category']; }, $array)) + 1;
+		if ( count( $array ) )
+		{
+			$lastCat = max(array_map(function($row) { return $row['category']; }, $array)) + 1;
+		}
 		
 		foreach ( $array as $item )
 		{